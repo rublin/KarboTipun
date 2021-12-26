e71ae81bb6b3fbe37afd32a5bc698c156e032ab2
rublin <clInS1L978je>
2021-12-26 09:05:47 +0200

    Initial commit

---
+++ src/main/kotlin/org/karbo/karbotipun/KarboTipunApplication.kt
@@ -0,0 +1,11 @@
+package org.karbo.karbotipun
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KarboTipunApplication
+
+fun main(args: Array<String>) {
+    runApplication<KarboTipunApplication>(*args)
+}
---
+++ src/test/kotlin/org/karbo/karbotipun/KarboTipunApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.karbo.karbotipun
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KarboTipunApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
